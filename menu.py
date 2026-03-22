339f8a6d7bff2ce46e5c3c60b01bee390a32dde3
Dhon Nobre <nobredhon@gmail.com>
2026-03-21 21:42:55 -0300

    beta-0.1

--- menu.py
+++ menu.py
@@ -10,7 +10,7 @@ conexao = mysql.connector.connect(
 )
 ger_aluno = GerenciadorAluno(conexao)
 ger_plano = GerenciadorPlano(conexao)
-ger_intrutor = GerenciadorInstrutor(conexao)
+ger_instrutor = GerenciadorInstrutor(conexao)
 while True:
     print("-" * 30)
     print("Menu principal")
@@ -94,15 +94,43 @@ while True:
                 op = input()
                 match op:
                     case "1":
-                        pass
+                        nome = input("Digite o nome do Instrutor que deseja adicionar!")
+                        email = input("Digite o E-mail do instrutor!")
+                        telefone = input("Digite o Telefone do instrutor!")
+                        cpf = input("Digite o CPF do instrutor!")
+                        especialidade = input("Digite a especialidade do instrutor")
+                        ger_instrutor.adicionando(nome, cpf, email, telefone, especialidade)
+                        print("Instrutor adicionado!")
                     case "2":
-                        ger_intrutor.listando()
+                        ger_instrutor.listando()
                     case "3":
                         name = input("Digite o nome do Instrutor que você deseja excluir!")
-                        ger_intrutor.limpando(name)
+                        ger_instrutor.limpando(name)
                         print("Instrutor excluido!")
                     case "4":
-                        pass
+                        campos_validos = {
+                            "1": "nome_instrutor",
+                            "2": "cpf_instrutor",
+                            "3": "email_instrutor",
+                            "4": "telefone_instrutor",
+                            "5": "especialidade"
+                        }
+                        nome = input("Digite o nome do Instrutor que deseja alterar")
+                        print("""
+                        1 - Nome
+                        2 - Email
+                        3 - CPF      
+                        4 - Telefone
+                        5 - Especialidade
+                        """)
+                        opcao = input("Escolha o campo: ")
+                        if opcao in campos_validos:
+                            campo = campos_validos[opcao]
+                            valor_alterado = input("Digite o novo valor: ")
+                            print(f"Alterando {campo} do instrutor {nome}")
+                            ger_instrutor.alterando(nome, campo, valor_alterado)
+                        else:
+                            print("Opção invalida")
                     case "0":
                         break
                     case _:
@@ -121,7 +149,12 @@ while True:
                 op = input()
                 match op:
                     case "1":
-                        pass
+                        nome = input("Digite o nome do Instrutor que deseja adicionar!")
+                        preco = input("Digite o E-mail do instrutor!")
+                        vantagens = input("Digite o Telefone do instrutor!")
+                        desvantagens = input("Digite o CPF do instrutor!")    
+                        ger_instrutor.adicionando(nome, preco, vantagens, desvantagens)
+                        print("Plano adicionado!")
                     case "2":
                         ger_plano.listando()
                     case "3":
@@ -129,7 +162,27 @@ while True:
                         ger_plano.limpando(name)
                         print("Plano excluido!")
                     case "4":
-                        pass
+                        campos_validos = {
+                            "1": "nome_plano",
+                            "2": "preco_plano",
+                            "3": "vantagens",
+                            "4": "desvantagens",
+                        }
+                        nome = input("Digite o nome do Instrutor que deseja alterar")
+                        print("""
+                        1 - Nome
+                        2 - Preço
+                        3 - Vantagens     
+                        4 - Desvantagens
+                        """)
+                        opcao = input("Escolha o campo: ")
+                        if opcao in campos_validos:
+                            campo = campos_validos[opcao]
+                            valor_alterado = input("Digite o novo valor: ")
+                            print(f"Alterando {campo} do plano {nome}")
+                            ger_plano.alterando(nome, campo, valor_alterado)
+                        else:
+                            print("Opção invalida")
                     case "0":
                         break
                     case _:
